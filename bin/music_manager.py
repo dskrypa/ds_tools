50fbc512cda38b2c944aba12f2500187e1647d55
dskrypa <dskrypa@gmail.com>
2018-08-12 15:39:55 -0400

    added tag copying function

--- bin/music_manager.py
+++ bin/music_manager.py
@@ -9,10 +9,13 @@ Manage music files
 import argparse
 import logging
 import os
+import pickle
 import string
 import sys
 from collections import Counter, defaultdict
 from fnmatch import fnmatch
+from hashlib import sha256
+from io import BytesIO
 from itertools import chain
 
 import grapheme
@@ -44,19 +47,33 @@ def _frame_repr(self):
 Frame.__repr__ = _frame_repr
 
 
+class FakeMusicFile:
+    def __init__(self, sha256sum, tags):
+        self.filename = sha256sum
+        self.tags = tags
+
+
 def main():
     parser = argparse.ArgumentParser(description="Music Manager")
     sparsers = parser.add_subparsers(dest="action", help="Action to perform")
 
     info_parser = sparsers.add_parser("info", help="Get song/tag information")
     info_parser.add_argument("path", nargs="+", help="One or more file/directory paths that contain music files")
-    stat_group = info_parser.add_mutually_exclusive_group()
-    stat_group.add_argument("--count", "-c", action="store_true", help="Count tag types rather than printing all info")
-    stat_group.add_argument("--unique", "-u", metavar="TAGID", nargs="+", help="Count unique values of the specified tag(s)")
+
+    info_group = info_parser.add_mutually_exclusive_group()
+    info_group.add_argument("--count", "-c", action="store_true", help="Count tag types rather than printing all info")
+    info_group.add_argument("--unique", "-u", metavar="TAGID", nargs="+", help="Count unique values of the specified tag(s)")
+    info_group.add_argument("--tags", "-t", nargs="+", help="Filter tags to display in file info mode")
 
     rm_parser = sparsers.add_parser("remove", help="Remove specified tags from the given files")
     rm_parser.add_argument("path", nargs="+", help="One or more file/directory paths that contain music files")
     rm_parser.add_argument("--tags", "-t", nargs="+", help="One or more tag IDs to remove from the given files")
+
+    cp_parser = sparsers.add_parser("copy", help="Copy specified tags from one set of files to another")
+    cp_parser.add_argument("--source", "-s", nargs="+", help="One or more file/directory paths that contain music files or ID3 info backups", required=True)
+    cp_parser.add_argument("--dest", "-d", nargs="+", help="One or more file/directory paths that contain music files, or a path to store an ID3 info backup")
+    cp_parser.add_argument("--backup", "-b", action="store_true", help="Store a backup of ID3 information instead of writing directly to matching music files")
+    cp_parser.add_argument("--tags", "-t", nargs="+", help="One or more tag IDs to copy to the destination files")
 
     for p in chain((parser,), sparsers.choices.values()):
         p.add_argument("--verbose", "-v", action="count", help="Print more verbose log info (may be specified multiple times to increase verbosity)")
@@ -71,12 +88,154 @@ def main():
         elif args.unique:
             count_unique_vals(args.path, args.unique)
         else:
-            print_song_tags(args.path)
+            print_song_tags(args.path, args.tags)
     elif args.action == "remove":
         remove_tags(args.path, args.tags, args.dry_run)
+    elif args.action == "copy":
+        if args.backup:
+            if len(args.dest) > 1:
+                raise ValueError("Only one --dest/-d path may be provided in backup mode")
+            save_tag_backups(args.source, args.dest)
+        else:
+            copy_tags(args.source, args.dest, args.tags, args.dry_run)
+    else:
+        log.error("Unconfigured action")
 
 
-def iter_music_files(paths):
+def copy_tags(source_paths, dest_paths, tags, dry_run):
+    if not tags:
+        raise ValueError("One or more tags or 'ALL' must be specified for --tags/-t")
+    tags = sorted({tag.upper() for tag in tags})
+    all_tags = "ALL" in tags
+    src_tags = load_tags(source_paths)
+
+    prefix, verb = ("[DRY RUN] ", "Would update") if dry_run else ("", "Updating")
+
+    for music_file in iter_music_files(dest_paths):
+
+        path = music_file.filename
+        content_hash = tagless_sha256sum(music_file)
+        if content_hash in src_tags:
+            if all_tags:
+                log.info("{}{} all tags in {}".format(prefix, verb, path))
+                if not dry_run:
+                    log.info("Saving changes to {}".format(path))
+                    src_tags[content_hash].save(path)
+            else:
+                do_save = False
+                for tag in tags:
+                    tag_name = tag_name_map.get(tag, "[unknown]")
+                    tag_str = "{} ({})".format(tag, tag_name)
+                    src_vals = src_tags[content_hash].getall(tag)
+                    if not src_vals:
+                        log.info("{}: No value found in source for {}".format(path, tag_str))
+                    else:
+                        dest_vals = music_file.tags.getall(tag)
+                        if src_vals == dest_vals:
+                            log.info("{}: Source and dest values match for {}".format(path, tag_str))
+                        else:
+                            dest_val_str = ", ".join(map(tag_repr, dest_vals))
+                            src_val_str = ", ".join(map(tag_repr, src_vals))
+                            log.info("{}{} {} in {} from [{}] to [{}]".format(prefix, verb, tag_str, path, dest_val_str, src_val_str))
+                            if not dry_run:
+                                music_file.tags.setall(tag, src_vals)
+                                do_save = True
+                if do_save:
+                    log.info("Saving changes to {}".format(path))
+                    music_file.tags.save(path)
+        else:
+            log.info("{}: sha256sum not found in source paths".format(music_file.filename))
+
+
+def save_tag_backups(source_paths, backup_path):
+    if isinstance(backup_path, (list, tuple)):
+        backup_path = backup_path[0]
+    if not backup_path:
+        backup_path = "/var/tmp/id3_tags.pickled"
+    log.info("backup_path: {}".format(backup_path))
+    if os.path.exists(backup_path) and os.path.isdir(backup_path):
+        backup_path = os.path.join(backup_path, "id3_tags.pickled")
+
+    if os.path.isfile(backup_path):
+        tag_info = load_tags([backup_path])
+    else:
+        tag_info = {}
+
+    for music_file in iter_music_files(source_paths):
+        content_hash = tagless_sha256sum(music_file)
+        log.debug("{}: {}".format(music_file.filename, content_hash))
+        tag_info[content_hash] = music_file.tags
+
+    with open(backup_path, "wb") as f:
+        log.info("Writing tag info to: {}".format(backup_path))
+        pickle.dump(tag_info, f)
+
+
+def load_tags(paths):
+    if isinstance(paths, str):
+        paths = [paths]
+
+    tag_info = {}
+    for path in paths:
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
+                for f in files:
+                    file_path = os.path.join(root, f)
+                    music_file = mutagen.File(file_path)
+                    if music_file:
+                        content_hash = tagless_sha256sum(music_file)
+                        log.debug("{}: {}".format(music_file.filename, content_hash))
+                        tag_info[content_hash] = music_file.tags
+                    else:
+                        with open(file_path, "rb") as f:
+                            try:
+                                tag_info.update(pickle.load(f))
+                            except Exception as e:
+                                log.debug("Unable to load tag info from file: {}".format(file_path))
+                            else:
+                                log.debug("Loaded pickled tag info from {}".format(file_path))
+        elif os.path.isfile(path):
+            music_file = mutagen.File(path)
+            if music_file:
+                content_hash = tagless_sha256sum(music_file)
+                log.debug("{}: {}".format(music_file.filename, content_hash))
+                tag_info[content_hash] = music_file.tags
+            else:
+                with open(path, "rb") as f:
+                    try:
+                        tag_info.update(pickle.load(f))
+                    except Exception as e:
+                        log.debug("Unable to load tag info from file: {}".format(path))
+                    else:
+                        log.debug("Loaded pickled tag info from {}".format(path))
+        else:
+            log.error("Invalid path: {}".format(path))
+
+    # tbl = Table(
+    #     SimpleColumn("Hash"), SimpleColumn("Tag"), SimpleColumn("Tag Name"), SimpleColumn("Value"), update_width=True
+    # )
+    # rows = []
+    # for sha256sum, tags in tag_info.items():
+    #     for tag, val in tags.items():
+    #         tag = tag[:4]
+    #         rows.append({
+    #             "Hash": sha256sum, "Tag": tag, "Value": tag_repr(val), "Tag Name": tag_name_map.get(tag, "[unknown]")
+    #         })
+    # tbl.print_rows(rows)
+
+    return tag_info
+
+
+def tagless_sha256sum(music_file):
+    with open(music_file.filename, "rb") as f:
+        tmp = BytesIO(f.read())
+
+    mutagen.File(tmp).tags.delete(tmp)
+    tmp.seek(0)
+    return sha256(tmp.read()).hexdigest()
+
+
+def iter_music_files(paths, include_backups=False):
     for path in paths:
         if os.path.isdir(path):
             for root, dirs, files in os.walk(path):
@@ -86,13 +245,29 @@ def iter_music_files(paths):
                     if music_file:
                         yield music_file
                     else:
-                        log.debug("Not a music file: {}".format(file_path))
+                        if include_backups:
+                            found_backup = False
+                            for sha256sum, tags in load_tags(file_path).items():
+                                found_backup = True
+                                yield FakeMusicFile(sha256sum, tags)
+                            if not found_backup:
+                                log.debug("Not a music file: {}".format(file_path))
+                        else:
+                            log.debug("Not a music file: {}".format(file_path))
         elif os.path.isfile(path):
             music_file = mutagen.File(path)
             if music_file:
                 yield music_file
             else:
-                log.debug("Not a music file: {}".format(path))
+                if include_backups:
+                    found_backup = False
+                    for sha256sum, tags in load_tags(path).items():
+                        found_backup = True
+                        yield FakeMusicFile(sha256sum, tags)
+                    if not found_backup:
+                        log.debug("Not a music file: {}".format(path))
+                else:
+                    log.debug("Not a music file: {}".format(path))
         else:
             log.error("Invalid path: {}".format(path))
 
@@ -151,9 +326,10 @@ def remove_tags(paths, tag_ids, dry_run):
         log.info("None of the provided files had the specified tags")
 
 
-def print_song_tags(paths):
+def print_song_tags(paths, tags):
+    tags = {tag.upper() for tag in tags} if tags else None
     i = 0
-    for music_file in iter_music_files(paths):
+    for music_file in iter_music_files(paths, include_backups=True):
         if i:
             print()
         print("{} (ID3v{}.{}):".format(music_file.filename, *music_file.tags.version[:2]))
@@ -163,13 +339,15 @@ def print_song_tags(paths):
             if len(tag) > 4:
                 tag = tag[:4]
 
-            val = str(val)
-            rows.append({
-                "Tag": tag,
-                "Tag Name": tag_name_map.get(tag, "[unknown]"),
-                "Value": val if len(val) < 200 else "(too long)"
-            })
-        tbl.print_rows(rows)
+            if not tags or (tag in tags):
+                val = str(val)
+                rows.append({
+                    "Tag": tag,
+                    "Tag Name": tag_name_map.get(tag, "[unknown]"),
+                    "Value": tag_repr(val)
+                })
+        if rows:
+            tbl.print_rows(rows)
         i += 1
 
 
