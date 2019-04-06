b9cc9973cbb22bff08e28bcbc85e06f355119979
dskrypa <dskrypa@gmail.com>
2019-04-06 15:19:09 -0400

    fixed setup packages

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name='ds_tools',
@@ -9,6 +8,6 @@ setup(
     author='Doug Skrypa',
     description='Misc Python 3 libraries and scripts',
     url='https://github.com/dskrypa/ds_tools',
-    packages=['ds_tools'],
+    packages=find_packages(),
     classifiers=['Programming Language :: Python :: 3']
 )
