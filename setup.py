9e4590d711b28afdd412a925e42945c29fe4c389
dskrypa <dskrypa@gmail.com>
2019-04-06 14:58:14 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='ds_tools',
+    version='2019.04.06-1',
+    author='Doug Skrypa',
+    description='Misc Python 3 libraries and scripts',
+    url='https://github.com/dskrypa/ds_tools',
+    packages=['ds_tools'],
+    classifiers=['Programming Language :: Python :: 3']
+)
