0b628d0d599c9659b0362bb9727bcadd9c923757
dskrypa <dskrypa@gmail.com>
2019-11-09 12:29:22 -0500

    added workaround for cython version missing

--- primes/primes.py
+++ primes/primes.py
@@ -4,11 +4,15 @@ import argparse
 import gzip
 import math
 import os
+import sys
 import time
 from array import array
 from bisect import bisect_left
 
-from cprimes import PrimeFinder32, PrimeFinder64    # PrimeFinder as PrimeFinderC,
+try:
+    from cprimes import PrimeFinder32, PrimeFinder64    # PrimeFinder as PrimeFinderC,
+except ImportError:
+    print('Error importing cython version of PrimeFinder', file=sys.stderr)
 
 PRIMES = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139]
 
