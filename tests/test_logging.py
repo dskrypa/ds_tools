b175fe9931be40d108d534494b54f1102621ecd0
dskrypa <dskrypa@gmail.com>
2020-01-12 16:32:02 -0500

    extended test case; renamed cleanup method

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -13,27 +13,30 @@ log = logging.getLogger(__name__)
 
 
 class LoggingInitTest(unittest.TestCase):
-    def _cleanup(self, *names):
+    def _cleanup_handlers(self, *names):
         for name in names:
             logger = logging.getLogger(name)
-            logger.handlers[0].close()
-            del logger.handlers[0]
+            while logger.handlers:
+                logger.handlers[0].close()
+                del logger.handlers[0]
 
     def test_log_path_name(self):
         this_file_name = Path(__file__).stem
         with tempfile.TemporaryDirectory() as tmp_dir:
             log_path = init_logging(filename_fmt='{prog}.log', file_dir=tmp_dir, names='test', streams=False)
             self.assertEqual(Path(log_path).name, '{}.log'.format(this_file_name))
-            self._cleanup('test')
+            self._cleanup_handlers('test')
 
     def test_log_path_uniq(self):
         this_file_name = Path(__file__).stem
         with tempfile.TemporaryDirectory() as tmp_dir:
             log_path_1 = init_logging(filename_fmt='{prog}{uniq}.log', file_dir=tmp_dir, names='test1', streams=False)
             log_path_2 = init_logging(filename_fmt='{prog}{uniq}.log', file_dir=tmp_dir, names='test2', streams=False)
+            log_path_3 = init_logging(filename_fmt='{prog}{uniq}.log', file_dir=tmp_dir, names='test3', streams=False)
             self.assertEqual(Path(log_path_1).stem, this_file_name)
             self.assertEqual(Path(log_path_2).stem, '{}-0'.format(this_file_name))
-            self._cleanup('test1', 'test2')
+            self.assertEqual(Path(log_path_3).stem, '{}-1'.format(this_file_name))
+            self._cleanup_handlers('test1', 'test2', 'test3')
 
 
 if __name__ == '__main__':
