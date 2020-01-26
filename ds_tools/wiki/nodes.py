cc4d906e2df800b7ac50d1fbf020db33da177cc8
dskrypa <dskrypa@gmail.com>
2020-01-26 16:08:42 -0500

    refactored nodes to have simpler names, and to parse sections more completely, while preserving lazy computation

--- ds_tools/wiki/nodes.py
+++ ds_tools/wiki/nodes.py
@@ -3,100 +3,261 @@
 """
 
 import logging
+import re
 import sys
 from collections import OrderedDict
 
 from wikitextparser import WikiText
 
+from ..compat import cached_property
 from .utils import strip_style
 
 __all__ = [
-    'WikiNode', 'LinkNode', 'ListNode', 'MixedStringNode', 'StringNode', 'TableNode', 'process_table', 'process_list',
-    'split_line'
+    'Node', 'BasicNode', 'CompoundNode', 'MappingNode', 'MixedNode', 'String', 'Link', 'List', 'Table', 'Template',
+    'Root', 'Section', 'as_node', 'extract_links'
 ]
 log = logging.getLogger(__name__)
 PY_LT_37 = sys.version_info.major == 3 and sys.version_info.minor < 7
+ordered_dict = OrderedDict if PY_LT_37 else dict            # 3.7+ dict retains insertion order; dict repr is cleaner
 
 
-class WikiNode:
-    def __init__(self, value):
-        self.value = value
+class Node:
+    def __init__(self, raw):
+        if isinstance(raw, str):
+            raw = WikiText(raw)
+        self.raw = raw
+
+    def stripped(self, *args, **kwargs):
+        return strip_style(self.raw.string, *args, **kwargs)
 
     def __repr__(self):
-        return f'<{type(self).__name__}({self.value!r})>'
+        return f'<{type(self).__name__}()>'
 
-    @classmethod
-    def for_text(cls, text):
-        if isinstance(text, str):
-            text = WikiText(text)
-        return split_line(text)
+    def pprint(self):
+        print(self.raw.pformat())
+
+
+class BasicNode(Node):
+    def __repr__(self):
+        return f'<{type(self).__name__}({self.raw!r})>'
+
+
+class CompoundNode(Node):
+    def __init__(self, raw):
+        super().__init__(raw)
+
+    @cached_property
+    def children(self):
+        return []
+
+    def __repr__(self):
+        return f'<{type(self).__name__}({self.children!r})>'
 
     def __iter__(self):
-        yield from self.value
+        yield from self.children
 
 
-class StringNode(WikiNode):
+class MappingNode(CompoundNode):
+    @cached_property
+    def children(self):
+        return ordered_dict()
+
+    def __setitem__(self, key, value):
+        self.children[key] = value
+
+    def __getitem__(self, item):
+        return self.children[item]
+
+
+class MixedNode(CompoundNode):
+    @cached_property
+    def children(self):
+        return extract_links(self.raw)
+
+
+class String(BasicNode):
+    def __init__(self, raw):
+        super().__init__(raw)
+        self.value = strip_style(self.raw.string)
+
+    def __repr__(self):
+        return f'<{type(self).__name__}({self.raw.string!r})>'
+
     def __str__(self):
         return self.value
 
 
-class LinkNode(WikiNode):
-    def __init__(self, wiki_link):
-        self.title = wiki_link.title    # target = title + fragment
-        self.text = wiki_link.text
-        super().__init__(wiki_link)
+class Link(BasicNode):
+    def __init__(self, raw):
+        super().__init__(raw)
+        self.link = raw.wikilinks[0]
+        self.title = self.link.title    # target = title + fragment
+        self.text = self.link.text
 
     def __repr__(self):
-        return f'<{type(self).__name__}({self.value.string!r})>'
+        return f'<{type(self).__name__}({self.link.string!r})>'
 
 
-class MixedStringNode(WikiNode):
-    pass
+class List(CompoundNode):
+    @cached_property
+    def children(self):
+        return [as_node(WikiText(val)) for val in map(str.strip, self.raw.items)]
 
 
-class ListNode(WikiNode):
-    def __init__(self, wiki_list):
-        super().__init__(process_list(wiki_list))
+class Table(CompoundNode):
+    @cached_property
+    def children(self):
+        rows = iter(self.raw.cells())
+        columns = [strip_style(cell.value) for cell in next(rows)]
+        processed = [ordered_dict(zip(columns, map(as_node, row))) for row in rows]
+        return processed
 
 
-class TableNode(WikiNode):
-    def __init__(self, table):
-        super().__init__(process_table(table))
+class Template(CompoundNode):
+    @cached_property
+    def children(self):
+        return []       # TODO
 
 
-def process_table(table):
-    _dict = OrderedDict if PY_LT_37 else dict               # 3.7+ dict retains insertion order; dict repr is cleaner
-    rows = iter(table.cells())
-    columns = [strip_style(cell.value) for cell in next(rows)]
-    processed = [_dict(zip(columns, map(split_line, row))) for row in rows]
-    return processed
+class Root(Node):
+    # Children = sections
+    def __init__(self, page_text):
+        if isinstance(page_text, str):
+            page_text = WikiText(page_text.replace('\xa0', ' '))
+        super().__init__(page_text)
+
+    @cached_property
+    def sections(self):
+        sections = iter(self.raw.sections)
+        root = Section(next(sections))
+        last_by_level = {0: root}
+        for sec in sections:
+            parent_lvl = sec.level - 1
+            while parent_lvl > 0 and parent_lvl not in last_by_level:
+                parent_lvl -= 1
+            parent = last_by_level[parent_lvl]
+            section = Section(sec)
+            parent.children[section.title] = section
+            last_by_level[section.level] = section
+        return root
 
 
-def process_list(wiki_list):
-    return [split_line(WikiText(val)) for val in map(str.strip, wiki_list.items)]
+class Section(Node):
+    def __init__(self, raw):
+        super().__init__(raw)
+        self.title = strip_style(raw.title)
+        self.level = raw.level
+        self.children = ordered_dict()
+
+    def __repr__(self):
+        return f'<{type(self).__name__}[{self.level}: {self.title}]>'
+
+    def __getitem__(self, item):
+        return self.children[item]
+
+    def find(self, title):
+        try:
+            return self.children[title]
+        except KeyError:
+            pass
+        for child in self.children.values():
+            try:
+                return child.find(title)
+            except KeyError:
+                pass
+        raise KeyError(f'Cannot find section={title!r} in {self} or any subsections')
+
+    @cached_property
+    def content(self):
+        if self.level == 0:
+            return as_node(self.raw)
+        return as_node(self.raw.string.partition('\n')[2])  # chop off the header
+
+    def pprint(self, mode='reprs', indent=''):
+        if mode == 'content':
+            print(self.raw.pformat())
+            for child in self.children.values():
+                child.pprint()
+        elif mode == 'headers':
+            print(f'{indent}{"=" * self.level}{self.title}{"=" * self.level}')
+            for child in self.children.values():
+                child.pprint(mode, indent=indent + ' ' * 4)
+        elif mode == 'reprs':
+            print(f'{indent}{self}')
+            for child in self.children.values():
+                child.pprint(mode, indent=indent + ' ' * 4)
 
 
-def split_line(wiki_text):
+def as_node(wiki_text):
     """
-    Split a line of WikiText into a list of links and strings.  String values will have any style tags stripped.
-
-    :param WikiText wiki_text: A WikiText object representing a single line
-    :return WikiNode: A list containing strings and/or WikiLink / WikiText objects
+    :param str|WikiText wiki_text: The content to process
+    :return Node: A :class:`Node` of subclass thereof
     """
-    as_str = strip_style(wiki_text.string)
+    if isinstance(wiki_text, str):
+        wiki_text = WikiText(wiki_text)
+
+    func_to_node = [
+        ('tables', Table), ('lists', List), ('tags', BasicNode), ('comments', BasicNode), ('templates', Template)
+    ]
+    for name, node_cls in func_to_node:
+        prop = getattr(wiki_text, name)
+        raw_objs = prop() if hasattr(prop, '__call__') else prop
+        if raw_objs:
+            node = node_cls(raw_objs[0])
+            if node.raw.string == wiki_text.string:
+                return node
+            compound = CompoundNode(wiki_text)
+            before, table_str, after = map(str.strip, wiki_text.string.partition(node.raw.string))
+            if before:
+                before_node = as_node(before)
+                if type(before_node) is CompoundNode:
+                    compound.children.extend(before_node.children)
+                else:
+                    compound.children.append(before_node)
+
+            compound.children.append(node)
+            if after:
+                after_node = as_node(after)
+                if type(after_node) is CompoundNode:
+                    compound.children.extend(after_node.children)
+                else:
+                    compound.children.append(after_node)
+
+            return compound
+
     links = wiki_text.wikilinks
     if not links:
-        return StringNode(as_str)
-    elif len(links) == 1 and links[0].string == as_str:
-        return LinkNode(links[0])
+        return String(wiki_text)
+    elif len(links) == 1 and links[0].string == strip_style(wiki_text.string):
+        return Link(wiki_text)
+
+    return MixedNode(wiki_text)
+
+
+def extract_links(raw):
+    try:
+        end_pat = extract_links._end_pat
+        start_pat = extract_links._start_pat
+    except AttributeError:
+        end_pat = extract_links._end_pat = re.compile(r'^(.*?)([\'"]+)$')
+        start_pat = extract_links._start_pat = re.compile(r'^([\'"]+)(.*)$')
 
     content = []
-    for link in links:
-        before, link_text, as_str = map(str.strip, as_str.partition(link.string))
+    raw_str = raw.string.strip()
+    for link in raw.wikilinks:
+        before, link_text, raw_str = map(str.strip, raw_str.partition(link.string))
+        if before and raw_str:
+            bm = end_pat.match(before)
+            if bm:
+                am = start_pat.match(raw_str)
+                if am:
+                    before = bm.group(1).strip()
+                    link_text = f'{bm.group(2)}{link_text}{am.group(1)}'
+                    raw_str = am.group(2).strip()
         if before:
-            content.append(before)
-        content.append(LinkNode(link))
+            content.append(String(before))
+        content.append(Link(WikiText(link_text)))
 
-    if as_str:
-        content.append(as_str)
-    return MixedStringNode(content)
+    if raw_str:
+        content.append(String(raw_str))
+    return content
--- ds_tools/wiki/page.py
+++ ds_tools/wiki/page.py
@@ -10,25 +10,21 @@ Notes:\n
 """
 
 import logging
-from collections import OrderedDict
-
-from wikitextparser import WikiText
 
 from ..compat import cached_property
-from .nodes import ListNode, WikiNode, TableNode
+from .nodes import Root, as_node, MappingNode
 from .utils import strip_style
 
-__all__ = ['WikiPage', 'WikiPageSection']
+__all__ = ['WikiPage']
 log = logging.getLogger(__name__)
 
 
-class WikiPage:
+class WikiPage(Root):
     def __init__(self, title, site, content, categories):
+        super().__init__(content)
         self.site = site
         self.title = title
-        self.content = content
         self.categories = categories
-        self.parsed = WikiText(content.replace('\xa0', ' '))    # \xa0 = non-breaking space / &nbsp;
 
     def __repr__(self):
         return f'<{type(self).__name__}[{self.title!r} @ {self.site}]>'
@@ -40,16 +36,15 @@ class WikiPage:
         further processing of links or other data structures.  Wiki lists are converted to Python lists of WikiText
         values.
         """
-        data = {}
-        infobox = self.parsed.templates[0]
-        for arg in infobox.arguments:
-            key = strip_style(arg.name)
-            list_vals = arg.lists()
-            if list_vals:
-                data[key] = ListNode(list_vals[0])
-            else:
-                data[key] = WikiNode.for_text(arg.value.strip())
-        return data
+        templates = self.raw.sections[0].templates
+        if templates:
+            infobox = templates[0]
+            node = MappingNode(infobox)
+            for arg in infobox.arguments:
+                key = strip_style(arg.name)
+                node[key] = as_node(arg.value.strip())
+            return node
+        return None
 
     @cached_property
     def intro(self):
@@ -57,12 +52,12 @@ class WikiPage:
         Neither parser provides access to the 1st paragraph directly when an infobox template precedes it - need to
         remove the infobox from the 1st section, or any other similar elements.
         """
-        intro_section = self.parsed.sections[0]
+        intro_section = self.raw.sections[0]
         templates = intro_section.templates
         if templates:
             infobox_end = templates[0].span[1]
             intro = intro_section[infobox_end:].strip()
-            return WikiNode.for_text(intro)
+            return as_node(intro)
 
         tags = intro_section.tags()
         if tags:
@@ -70,90 +65,4 @@ class WikiPage:
             intro = intro_section.string.partition(tag)[2]
         else:
             intro = intro_section.string
-
-        return WikiNode.for_text(intro)
-
-    @cached_property
-    def sections(self):
-        """Both parsers provide a flat list of sections - this provides a view of them nested by level"""
-        sections = iter(self.parsed.sections)
-        root = WikiPageSection(next(sections))
-        last_by_level = {0: root}
-        for sec in sections:
-            parent_lvl = sec.level - 1
-            while parent_lvl > 0 and parent_lvl not in last_by_level:
-                parent_lvl -= 1
-            parent = last_by_level[parent_lvl]
-            section = parent.add(sec)
-            last_by_level[section.level] = section
-        return root
-
-
-class WikiPageSection:
-    """
-    A section in a MediaWiki page.  Improves accessibility of nested subsections.  This class should not be instantiated
-    directly - it is built by :meth:`WikiPage.sections`.
-
-    Stores the :class:`Section<wikitextparser.Section>` object that contains the real section data.
-    """
-    def __init__(self, section, parent=None):
-        self.title = strip_style(section.title).strip()
-        self.level = section.level
-        self.raw_content = section
-        self.children = OrderedDict()
-        self.parent = parent
-
-    def __repr__(self):
-        return f'<{type(self).__name__}[{self.level}: {self.title}]>'
-
-    def __getitem__(self, item):
-        return self.children[item]
-
-    def add(self, section):
-        sub = WikiPageSection(section, self)
-        self.children[sub.title] = sub
-        return sub
-
-    def find(self, title):
-        try:
-            return self.children[title]
-        except KeyError:
-            pass
-        for child in self.children.values():
-            try:
-                return child.find(title)
-            except KeyError:
-                pass
-        raise KeyError(f'Cannot find section={title!r} in {self} or any subsections')
-
-    def content(self):
-        """
-        WIP - return this section's content, processed into a form that makes it easier to work with
-        """
-        tables = self.raw_content.tables
-        if tables:
-            if len(tables) == 1:
-                return TableNode(tables[0])
-            return [TableNode(t) for t in tables]
-
-        lists = self.raw_content.lists()
-        if lists:
-            if len(lists) == 1:
-                return ListNode(lists[0])
-            return [ListNode(lst) for lst in lists]
-
-        return self.raw_content
-
-    def pprint(self, mode='reprs', indent=''):
-        if mode == 'content':
-            print(self.raw_content.pformat())
-            for child in self.children.values():
-                child.pprint()
-        elif mode == 'headers':
-            print(f'{indent}{"=" * self.level}{self.title}{"=" * self.level}')
-            for child in self.children.values():
-                child.pprint(mode, indent=indent + ' ' * 4)
-        elif mode == 'reprs':
-            print(f'{indent}{self}')
-            for child in self.children.values():
-                child.pprint(mode, indent=indent + ' ' * 4)
+        return as_node(intro)
