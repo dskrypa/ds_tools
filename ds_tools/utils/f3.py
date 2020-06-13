f49b5d951c692b4fe369bbe680f9db2273acfb28
dskrypa <dskrypa@gmail.com>
2020-06-13 09:51:58 -0400

    moved f3 funcs into a module; made a class to keep buffer between files to prevent multiple large mallocs

---
+++ ds_tools/utils/f3.py
@@ -0,0 +1,160 @@
+"""
+Utilities for generating disk test data, based on F3: https://github.com/AltraMayor/f3
+
+:author: Doug Skrypa
+"""
+
+import logging
+import time
+from enum import Enum
+from errno import ENOSPC
+from pathlib import Path
+from shutil import disk_usage
+from typing import Union
+
+import cffi
+
+from tz_aware_dt import format_duration
+from ..output import readable_bytes
+
+__all__ = ['DEFAULT_CHUNK_SIZE', 'GB_BYTES', 'F3Data', 'F3Mode']
+log = logging.getLogger(__name__)
+
+GB_BYTES = 1073741824
+DEFAULT_CHUNK_SIZE = 1 << 21    # 2MB
+
+ffi = cffi.FFI()
+ffi.cdef('static uint64_t fill_buffer(void *buf, size_t size, uint64_t offset);')
+fill_buffer = ffi.verify("""
+#define SECTOR_SIZE 512
+#define GIGABYTES 1073741824
+
+static inline uint64_t random_number(uint64_t prv_number)
+{
+	return prv_number * 4294967311ULL + 17;
+}
+
+static uint64_t fill_buffer(void *buf, size_t size, uint64_t offset)
+{
+	const int num_int64 = SECTOR_SIZE >> 3;
+	uint8_t *p, *ptr_end;
+
+	assert(size > 0);
+	assert(size % SECTOR_SIZE == 0);
+
+	p = buf;
+	ptr_end = p + size;
+	while (p < ptr_end) {
+		uint64_t *sector = (uint64_t *)p;
+		int i;
+		sector[0] = offset;
+		for (i = 1; i < num_int64; i++)
+			sector[i] = random_number(sector[i - 1]);
+		p += SECTOR_SIZE;
+		offset += SECTOR_SIZE;
+	}
+
+	return offset;
+}
+""").fill_buffer
+
+
+class F3Mode(Enum):
+    ITER = 'iter'
+    FULL = 'full'
+
+
+class F3Data:
+    def __init__(self, mode, size: int = GB_BYTES, chunk_size: int = DEFAULT_CHUNK_SIZE):
+        self.mode = F3Mode(mode)
+        if chunk_size > size:
+            chunk_size = size
+        self.size = size
+        self.chunk_size = chunk_size
+        self.buf = bytearray(size if self.mode == F3Mode.FULL else chunk_size)
+        self.view = memoryview(self.buf)
+        self._num = None
+
+    def file(self, num: int):
+        self._num = num
+        return self
+
+    def __enter__(self):
+        assert self._num is not None
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._num = None
+
+    def __iter__(self):
+        assert self._num is not None
+        num = self._num - 1
+        size, chunk_size = self.size, self.chunk_size
+        view, buf = self.view, self.buf
+        offset = num * self.size
+        from_buffer = ffi.from_buffer
+        for start in range(0, size, chunk_size):
+            offset = fill_buffer(from_buffer(view), chunk_size, offset)
+            yield buf
+
+    def data(self, num: int):
+        size, chunk_size, view = self.size, self.chunk_size, self.view
+        offset = (num - 1) * size
+        from_buffer = ffi.from_buffer
+        for start in range(0, size, chunk_size):
+            end = start + chunk_size
+            chunk = view[start:end]
+            offset = fill_buffer(from_buffer(chunk), chunk_size, offset)
+        return self.buf
+
+    def _write_file(self, dir_path: Path, num: int):
+        path = dir_path.joinpath(f'{num}.h2w')
+        print(f'Writing file {path.name} ... ', end='', flush=True)
+        mode = self.mode
+        if mode == F3Mode.FULL:
+            data = self.data(num)
+            with path.open('wb') as f:
+                f.write(data)
+        elif mode == F3Mode.ITER:
+            with self.file(num) as f3_iter, path.open('wb') as f:
+                for chunk in f3_iter:
+                    f.write(chunk)
+
+    def write_files(self, path: Union[str, Path], start, end=None):
+        path = Path(path).resolve()
+        if not path.exists():
+            path.mkdir(parents=True)
+
+        free = disk_usage(path)[2]
+        end = end if end is not None else (free // GB_BYTES)
+        if end < start:
+            raise ValueError('end must be greater than start')
+        size = self.size
+        total = end - start + 1
+        log.info(f'Writing {total:,d} files to {path} [free space: {readable_bytes(free)}]\n')
+        start_time = time.monotonic()
+        for i, num in enumerate(range(start, end + 1), 1):
+            try:
+                self._write_file(path, num)
+            except OSError as e:
+                if e.errno == ENOSPC:
+                    log.info(f'OK (No space left in {path})')
+                    return True
+                else:
+                    print('ERROR')
+                    log.error(f'Unexpected error:', exc_info=True)
+                    return False
+            except Exception:
+                print('ERROR')
+                log.error(f'Unexpected error:', exc_info=True)
+                return False
+            else:
+                elapsed = time.monotonic() - start_time
+                bps = (i * size / elapsed) if elapsed else 0
+                remaining = ((total - i) * size / bps) if bps else 0
+                log.info(
+                    f'OK [Elapsed: {format_duration(elapsed)}] [{readable_bytes(bps)}/s] '
+                    f'[Est. Remaining: {format_duration(remaining)}]'
+                )
+        else:
+            return True
