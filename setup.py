87f64ddd6cd42a9759d3d34ad94f65f161149bd2
dskrypa <dskrypa@gmail.com>
2020-07-02 11:17:32 -0400

    added hooks command to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+from distutils import log
+from distutils.cmd import Command
+from distutils.errors import DistutilsOptionError, DistutilsExecError
 from itertools import chain
 from pathlib import Path
 from setuptools import setup, find_packages
+from subprocess import Popen
 
 project_root = Path(__file__).resolve().parent
 
@@ -14,6 +18,10 @@ with project_root.joinpath('ds_tools', '__version__.py').open('r', encoding='utf
     exec(f.read(), about)
 
 optional_dependencies = {
+    'dev': [                                            # Development env requirements
+        'pre-commit',
+        'ipython',
+    ],
     'cffi': ['cffi'],                                   # bin/cffi_test.py, bin/f3.py, examples/cffi/*
     'translate': ['googletrans'],                       # ds_tools.unicode.translate
     'J2R': ['pykakasi'],                                # ds_tools.unicode.languages
@@ -38,6 +46,29 @@ requirements = [
 ]
 
 
+class SetupHooksCmd(Command):
+    long_description = 'Manage pre-commit hooks'
+    user_options = [('install', 'i', 'Install pre-commit hooks'), ('uninstall', 'u', 'Uninstall pre-commit hooks')]
+    boolean_options = ['install', 'uninstall']
+
+    def initialize_options(self):
+        self.install = None
+        self.uninstall = None
+
+    def finalize_options(self):
+        options = (self.install, self.uninstall)
+        if all(val is None for val in options) or all(val is not None for val in options):
+            raise DistutilsOptionError('You must specify either --install xor --uninstall for pre-commit hooks')
+
+    def run(self):
+        cmd = 'install' if self.install else 'uninstall'
+        self.announce('Running pre-commit {}...'.format(cmd), log.INFO)
+        code = Popen(['pre-commit', cmd]).wait()
+        self.announce('Result: {}'.format(code), log.DEBUG)
+        if code != 0:
+            raise DistutilsExecError('Error: pre-commit {} exited with a non-zero status: {}'.format(cmd, code))
+
+
 setup(
     name=about['__title__'],
     version=about['__version__'],
@@ -54,5 +85,6 @@ setup(
     ],
     python_requires='~=3.8',
     install_requires=requirements,
-    extras_require=optional_dependencies
+    extras_require=optional_dependencies,
+    cmdclass={'hooks': SetupHooksCmd},
 )
