46459531a81a51795e167ea371ded3b03c7763d7
dskrypa <dskrypa@gmail.com>
2020-07-05 11:52:01 -0400

    removed unnecessary setup command for pre-commit; moved incr_version to separate repo & installed it properly

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
-from distutils import log
-from distutils.cmd import Command
-from distutils.errors import DistutilsOptionError, DistutilsExecError
 from itertools import chain
 from pathlib import Path
 from setuptools import setup, find_packages
-from subprocess import Popen
 
 project_root = Path(__file__).resolve().parent
 
@@ -22,8 +18,7 @@ scripts.append('incr_version.py')
 
 optional_dependencies = {
     'dev': [                                            # Development env requirements
-        'pre-commit',
-        'psutil',
+        'pre-commit',                                   # run `pre-commit install` to install hooks
         'ipython',
     ],
     'cffi': ['cffi'],                                   # bin/cffi_test.py, bin/f3.py, examples/cffi/*
@@ -50,29 +45,6 @@ requirements = [
 ]
 
 
-class SetupHooksCmd(Command):
-    long_description = 'Manage pre-commit hooks'
-    user_options = [('install', 'i', 'Install pre-commit hooks'), ('uninstall', 'u', 'Uninstall pre-commit hooks')]
-    boolean_options = ['install', 'uninstall']
-
-    def initialize_options(self):
-        self.install = None
-        self.uninstall = None
-
-    def finalize_options(self):
-        options = (self.install, self.uninstall)
-        if all(val is None for val in options) or all(val is not None for val in options):
-            raise DistutilsOptionError('You must specify either --install xor --uninstall for pre-commit hooks')
-
-    def run(self):
-        cmd = 'install' if self.install else 'uninstall'
-        self.announce('Running pre-commit {}...'.format(cmd), log.INFO)
-        code = Popen(['pre-commit', cmd]).wait()
-        self.announce('Result: {}'.format(code), log.DEBUG)
-        if code != 0:
-            raise DistutilsExecError('Error: pre-commit {} exited with a non-zero status: {}'.format(cmd, code))
-
-
 setup(
     name=about['__title__'],
     version=about['__version__'],
@@ -90,6 +62,5 @@ setup(
     python_requires='~=3.8',
     install_requires=requirements,
     extras_require=optional_dependencies,
-    cmdclass={'hooks': SetupHooksCmd},
     scripts=scripts,
 )
