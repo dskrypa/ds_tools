c9601139a59dac22a9e1f5cd17764dc017b4bf0e
dskrypa <dskrypa@gmail.com>
2020-09-19 08:52:20 -0400

    removed rarely used modules from package-level exposure

--- ds_tools/caching/__init__.py
+++ ds_tools/caching/__init__.py
@@ -4,7 +4,4 @@ A ``cached`` memoizing decorator and utilities that go with it, as well as some 
 :author: Doug Skrypa
 """
 
-from .exceptions import *
-from .caches import *
 from .decorate import *
-from .mixins import *
