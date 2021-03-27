710145990f38ff9c60bdaa2b6154ab5930a3ce14
dskrypa <dskrypa@gmail.com>
2021-03-27 08:03:32 -0400

    refactored subtitle offset script to remove unnecessary lib dependency + misc cleanup

--- bin/subtitle_offset.py
+++ bin/subtitle_offset.py
@@ -1,64 +1,53 @@
 #!/usr/bin/env python
 
-import sys
-from pathlib import Path
-
-sys.path.insert(0, Path(__file__).resolve().parents[1].joinpath('bin').as_posix())
-import _venv  # This will activate the venv, if it exists and is not already active
-
-import logging
+from argparse import ArgumentParser
 from datetime import datetime, timedelta
-
-from ds_tools.argparsing import ArgParser
-from ds_tools.logging import init_logging
-
-log = logging.getLogger(__name__)
+from pathlib import Path
+from typing import Iterable
 
 
 def parser():
-    parser = ArgParser(description='Subtitle Offset Adjuster')
-    parser.add_argument('subtitle_file', metavar='PATH', help='Path to the subtitle file to modify')
+    parser = ArgumentParser(description='Subtitle Offset Adjuster')
+    parser.add_argument('subtitle_file', metavar='PATH', help='Path to the subtitle file to modify (timestamps will be read from .bkp file with the same name if it already exists)')
     parser.add_argument('--offset', '-o', type=float, required=True, help='Offset in seconds')
-    parser.include_common_args('verbosity')
     return parser
 
 
 def main():
     args = parser().parse_args()
-    init_logging(args.verbose, log_path=None)
 
-    original = Path(args.subtitle_file)
-    backup = Path(args.subtitle_file + '.bkp')
+    orig_path = Path(args.subtitle_file)
+    backup_path = Path(args.subtitle_file + '.bkp')
 
-    path = backup if backup.exists() else original
-    revised = update_timestamps(path, args.offset)
+    read_path = backup_path if backup_path.exists() else orig_path
+    with read_path.open('r', encoding='utf-8') as f:
+        original = f.read().splitlines()
 
-    if not backup.exists():
-        log.info(f'Saving backup: {backup.as_posix()}')
-        original.rename(backup)
+    revised = update_timestamps(original, timedelta(seconds=args.offset))
 
-    with original.open('w', encoding='utf-8') as f:
+    if not backup_path.exists():
+        print(f'Saving backup: {backup_path.as_posix()}')
+        orig_path.rename(backup_path)
+
+    with orig_path.open('w', encoding='utf-8') as f:
         f.write('\n'.join(revised))
 
-    log.info(f'Updated {original.as_posix()}')
+    print(f'Updated {orig_path.as_posix()}')
 
 
-def update_timestamps(path, offset: float):
+def update_timestamps(original: Iterable[str], offset: timedelta):
     fmt = '%H:%M:%S,%f'
 
-    with open(path, 'r', encoding='utf-8') as f:
-        lines = f.read().splitlines()
-
     revised = []
-    for line in map(str.strip, lines):
+    for line in map(str.strip, original):
         try:
             a, b = line.split(' --> ')
-        except Exception:
+        except ValueError:
             revised.append(line)
         else:
             # print(f'Processing a={a!r} b={b!r}')
-            a = datetime.strptime(a, fmt) + timedelta(seconds=offset)
-            b = datetime.strptime(b, fmt) + timedelta(seconds=offset)
+            a = datetime.strptime(a, fmt) + offset
+            b = datetime.strptime(b, fmt) + offset
             revised.append(f'{a.strftime(fmt)[:-3]} --> {b.strftime(fmt)[:-3]}')
 
     return revised
