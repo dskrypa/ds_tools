1b41dcba5893448010b8232fdb1555e8fd303d14
dskrypa <dskrypa@gmail.com>
2021-08-05 08:03:32 -0400

    added func to help see what a given bytes object would be as unpacked structs

--- ds_tools/misc/binary.py
+++ ds_tools/misc/binary.py
@@ -4,6 +4,8 @@ Misc utilities related to working with binary data.
 :author: Doug Skrypa
 """
 
+from enum import Enum
+from struct import unpack_from, error as StructError
 from typing import Union, Iterator
 
 
@@ -29,3 +31,40 @@ class BytePattern:
 
     def all_matches(self, data: bytes) -> tuple[tuple[int, bytes], ...]:
         return tuple(self.iter_matches(data))
+
+
+class Endian(Enum):
+    NATIVE = '@'            # Native byte order, native size, native alignment
+    NATIVE_STANDARD = '='   # Native byte order, standard size
+    LITTLE = '<'            # Little-endian byte order, standard size
+    BIG = '>'               # Big-endian byte order, standard size
+
+    @classmethod
+    def _missing_(cls, value):
+        return cls._member_map_.get(value.upper() if isinstance(value, str) else value)
+
+
+FORMATS = {
+    '?': ('bool', 1),
+    'b': ('int8', 1),   'h': ('int16', 2),      'i': ('int32', 4),      'q': ('int64', 8),
+    'B': ('uint8', 1),  'H': ('uint16', 2),     'I': ('unit32', 4),     'Q': ('uint64', 8),
+                        'e': ('float16', 2),    'f': ('float32', 4),    'd': ('float64', 8),
+    # 'x': ('pad byte', 1), 'c': ('char', 1), 'l': ('int32', 4), 'L': ('uint32', 4), 's': ('string', 1),
+    # 'n': ('ssize_t', calcsize('n')), 'N': ('size_t', calcsize('N')), 'P': ('pointer', calcsize('P')),  # @ only
+    # 'p': ('pascal string', 1),  # 1st byte = min(len,255). struct only supports 255 char read, 254 char write.
+}
+
+
+def view_unpacked(data: bytes, *, split: int = 4, sep: str = ' ', offset: int = 0, endian: Endian = None):
+    endian = Endian(endian or '@')
+    unpacked = {'bin': sep.join(map('{:08b}'.format, data)), 'hex': data.hex(sep, split)}
+    for fc, (name, width) in FORMATS.items():
+        fmt = endian.value + fc
+        from_struct = []
+        for i in range(offset, len(data), width):
+            try:
+                from_struct.extend(unpack_from(fmt, data, i))
+            except StructError:
+                pass
+        unpacked[name] = from_struct
+    return unpacked
