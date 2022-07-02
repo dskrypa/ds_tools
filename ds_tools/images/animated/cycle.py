55ef14faa1fcda4a906a7d33aa5b1c7f0ced4132
dskrypa <dskrypa@gmail.com>
2022-07-02 17:38:24 -0400

    another attempt at resizing gifs

--- ds_tools/images/animated/cycle.py
+++ ds_tools/images/animated/cycle.py
@@ -9,9 +9,10 @@ from __future__ import annotations
 import logging
 from pathlib import Path
 from tkinter import PhotoImage
-from typing import Iterator, Iterable, Callable, Union, TypeVar, Generic
+from typing import Iterator, Iterable, Callable, Union, TypeVar, Generic, Optional, Sequence
 
-from PIL.Image import Image as PILImage
+from PIL.GifImagePlugin import GifImageFile
+from PIL.Image import Image as PILImage, new as new_image
 from PIL.ImageSequence import Iterator as FrameIterator
 from PIL.ImageTk import PhotoImage as PilPhotoImage
 
@@ -28,24 +29,31 @@ class FrameCycle(Generic[T_co]):
 
     def __init__(
         self,
-        frames: Iterable[PILImage],
+        frames: Sequence[Union[PILImage, tuple[PILImage, Optional[int]]]],
         wrapper: Callable[[PILImage], T_co] = None,
         duration: int = None,
         default_duration: int = 100,
     ):
         self.n = 0
-        self._frames = tuple(frames)
         self._wrapper = wrapper
         self._duration = duration
         self._default_duration = default_duration
 
-        def get_duration(f):
-            return duration if duration is not None else f.info.get('duration', default_duration)
-
-        if wrapper is None:
-            self._frames_and_durations = tuple((f, get_duration(f)) for f in self._frames)
+        if isinstance(frames[0], tuple):
+            self._frames = tuple(f[0] for f in frames)
+            self._frames_and_durations = tuple(
+                (f, duration if duration else (default_duration if d is None else d)) for f, d in frames
+            )
         else:
-            self._frames_and_durations = tuple((wrapper(f), get_duration(f)) for f in self._frames)
+            self._frames = tuple(frames)
+
+            def get_duration(f):
+                return duration if duration is not None else f.info.get('duration', default_duration)
+
+            if wrapper is None:
+                self._frames_and_durations = tuple((f, get_duration(f)) for f in self._frames)
+            else:
+                self._frames_and_durations = tuple((wrapper(f), get_duration(f)) for f in self._frames)
 
         self.first_delay = self._frames_and_durations[0][1]
 
@@ -81,8 +89,84 @@ class FrameCycle(Generic[T_co]):
 
     def resized(self, width: int, height: int) -> FrameCycle[PilPhotoImage]:
         size = (width, height)
-        frames = [frame.copy().resize(size) for frame in self._frames]
+        mode = _get_mode(self._frames)  # noqa
+        raw_frames = iter_independent_frames(self._frames, mode)  # noqa
+        frames = [
+            (frame.resize(size), duration) for frame, (_, duration) in zip(raw_frames, self._frames_and_durations)
+        ]
+        # frames = [frame.copy().resize(size) for frame in self._frames]
         return FrameCycle(frames, PilPhotoImage, self._duration, self._default_duration)
+
+
+def _get_mode(frames: Iterable[GifImageFile]) -> str:
+    mode = 'full'
+    for frame in frames:
+        try:
+            if frame.tile:
+                tile = frame.tile[0]
+                update_region = tile[1]
+                update_region_dimensions = update_region[2:]
+                if update_region_dimensions != frame.size:
+                    mode = 'partial'
+                    break
+        except AttributeError:
+            break
+
+    return mode
+
+
+def _iter_raw_frames(image: GifImageFile) -> Iterator[GifImageFile]:
+    try:
+        while True:
+            yield image
+            image.seek(image.tell() + 1)
+    except EOFError:
+        pass
+
+
+def get_mode(image: GifImageFile) -> str:
+    mode = _get_mode(_iter_raw_frames(image))
+    image.seek(0)
+    return mode
+
+
+def iter_independent_frames(frames: Iterable[GifImageFile], mode: str) -> Iterator[PILImage]:
+    frames = iter(frames)
+    first = next(frames)
+    p = first.getpalette()
+    prev_frame = first.convert('RGBA')
+    yield first
+    for frame in frames:
+        if not frame.getpalette():
+            frame.putpalette(p)
+
+        new_frame = new_image('RGBA', frame.size)
+        if mode == 'partial':
+            new_frame.paste(prev_frame)
+        new_frame.paste(frame, (0, 0), frame.convert('RGBA'))  # noqa
+        yield new_frame
+        prev_frame = new_frame
+
+
+def iter_frames(image: GifImageFile) -> Iterator[tuple[PILImage, Optional[int]]]:
+    # Based on https://gist.github.com/almost/d2832d0998ad9dfec2cacef934e7d247
+    mode = get_mode(image)
+    p = image.getpalette()
+    prev_frame = image.convert('RGBA')
+    try:
+        while True:
+            if not image.getpalette():
+                image.putpalette(p)
+
+            new_frame = new_image('RGBA', image.size)
+            if mode == 'partial':
+                new_frame.paste(prev_frame)
+            new_frame.paste(image, (0, 0), image.convert('RGBA'))  # noqa
+            yield new_frame, image.info.get('duration')
+            prev_frame = new_frame
+            image.seek(image.tell() + 1)
+    except EOFError:
+        pass
 
 
 class PhotoImageCycle(FrameCycle[PhotoImage]):
