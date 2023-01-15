3486a353abe9bec5ef71ff93a91f76b4947fd0d0
dskrypa <dskrypa@gmail.com>
2023-01-15 11:38:41 -0500

    added past participle to logging prefix

--- ds_tools/output/prefix.py
+++ ds_tools/output/prefix.py
@@ -4,24 +4,41 @@ Dry/normal run verb prefixes for user-facing log messages.
 
 from __future__ import annotations
 
+from enum import Enum
 from typing import Optional, Type, Union
 
 __all__ = ['LoggingPrefix', 'Verb']
 
 
+class Tense(Enum):
+    PRESENT = 'present'
+    PAST = 'past'
+
+    @classmethod
+    def _missing_(cls, value):
+        if isinstance(value, str):
+            try:
+                return cls._member_map_[value.upper()]
+            except KeyError:
+                pass
+        return super()._missing_(value)
+
+
 class Verb:
-    __slots__ = ('base', '_present_participle', 'double_last', 'drop_last')
+    __slots__ = ('base', '_present_participle', '_past_participle', 'double_last', 'drop_last')
 
     def __init__(
         self,
         base: str = None,
         *,
         present_participle: str = None,  # -ing
+        past_participle: str = None,  # -ed
         double_last: bool = False,
         drop_last: bool = False,
     ):
         self.base = base
         self._present_participle = present_participle
+        self._past_participle = past_participle
         self.double_last = double_last
         self.drop_last = drop_last
 
@@ -40,22 +57,42 @@ class Verb:
             base = base[:-1]
         return base + 'ing'
 
-    def conjugate(self, dry_run: bool = False) -> str:
-        if not dry_run:
+    @property
+    def past_participle(self) -> str:
+        if self._past_participle:
+            return self._past_participle
+        base = self.base
+        suffix = 'd' if base.endswith('e') else 'ed'
+        return base + suffix
+
+    def conjugate(self, dry_run: bool = False, tense: Tense = Tense.PRESENT) -> str:
+        if dry_run:
+            return f'[DRY RUN] Would {self.base}'
+        elif tense == Tense.PRESENT:
             return self.present_participle.capitalize()
-        return f'[DRY RUN] Would {self.base}'
+        else:
+            return self.past_participle.capitalize()
 
     def __get__(self, instance: Optional[LoggingPrefix], owner: Type[LoggingPrefix]) -> Union[Verb, str]:
         if instance is None:
             return self
-        return self.conjugate(instance.dry_run)
+        return self.conjugate(instance.dry_run, instance.tense)
 
 
 class LoggingPrefix:
-    __slots__ = ('dry_run',)
+    __slots__ = ('dry_run', '_tense')
 
-    def __init__(self, dry_run: bool = False):
+    def __init__(self, dry_run: bool = False, tense: Tense | str = Tense.PRESENT):
         self.dry_run = dry_run
+        self.tense = tense
+
+    @property
+    def tense(self) -> Tense:
+        return self._tense
+
+    @tense.setter
+    def tense(self, value: Tense | str):
+        self._tense = Tense(value)
 
     def __getitem__(self, verb: str) -> str:
         try:
@@ -66,5 +103,5 @@ class LoggingPrefix:
     add = Verb()
     create = Verb(drop_last=True)
     delete = Verb(drop_last=True)
-    run = Verb(double_last=True)
+    run = Verb(double_last=True, past_participle='ran')
     save = Verb(drop_last=True)
