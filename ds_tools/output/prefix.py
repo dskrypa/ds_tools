c185485deb6767888ca28b34d555a109622623d1
dskrypa <dskrypa@gmail.com>
2023-02-20 13:43:53 -0500

    added verb

--- ds_tools/output/prefix.py
+++ ds_tools/output/prefix.py
@@ -120,6 +120,7 @@ class LoggingPrefix:
     create = Verb(drop_last=True)
     delete = Verb(drop_last=True)
     move = Verb(drop_last=True)
+    remove = Verb(drop_last=True)
     reset = Verb(double_last=True, past_participle='reset')
     run = Verb(double_last=True, past_participle='ran')
     save = Verb(drop_last=True)
